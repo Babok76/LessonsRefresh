2c46e2de0aac9671db9efa6765f87ece537cbe1a
Babok76 <skv767676@rambler.ru>
2023-08-28 18:09:51 +0300

    6

---
+++ src/Les28_Modificators/Les28_Test.java
@@ -0,0 +1,18 @@
+package Les28_Modificators;
+
+import Les28_Modificators.Packet1.Person;
+
+public class Les28_Test { //класс не может быть приватным/private или protected
+    public int id;
+    protected int id2; // доступен если наследуется от класса
+
+    public static void main(String[] args) {
+        //public, private, default (доступно в пределах папки/packege), protected (доступно в пределах папки/packege и в подклассах вне пакета)
+
+        Person pers1 = new Person();
+        //pers1.name = "bob";
+        System.out.println(pers1.bax);
+        //System.out.println(pers1.name1);
+    }
+}
+
---
+++ src/Les28_Modificators/Packet1/Person.java
@@ -0,0 +1,10 @@
+package Les28_Modificators.Packet1;
+
+public class Person {
+    private String name;
+    public final static String bax = "sdfd";
+    protected String name1;
+    public Person () {
+        name1="bob";
+    }
+}
