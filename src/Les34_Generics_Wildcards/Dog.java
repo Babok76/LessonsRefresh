383f2ddce3e80db3ab0132fe9f3e4bceaed88086
Babok76 <skv767676@rambler.ru>
2023-08-30 19:04:52 +0300

    6

---
+++ src/Les34_Generics_Wildcards/Dog.java
@@ -0,0 +1,8 @@
+package Les34_Generics_Wildcards;
+
+public class Dog extends Animal{
+
+    public void Gau(){
+        System.out.println("Gau!Gau!");
+    }
+}
---
+++ src/Les34_Generics_Wildcards/Test.java
@@ -0,0 +1,34 @@
+package Les34_Generics_Wildcards;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test { //WildCard - это полиморфизм для ArrayList
+    public static void main(String[] args) {
+        List<Animal> listOfAnimals = new ArrayList<>();
+        listOfAnimals.add(new Animal(1));
+        listOfAnimals.add(new Animal(2));
+
+        test(listOfAnimals);
+
+        List<Dog> listOfDogs = new ArrayList<>();
+        listOfDogs.add(new Dog());
+        listOfDogs.add(new Dog());
+
+        test(listOfDogs);
+
+        //Object object = new Object();
+        //Object - иерархия наследования
+        //Animal
+        //Dog
+
+    }
+
+    //private static void test(List<Animal> list) {
+    private static void test(List<? extends /*super*/ Animal>  list) { // super будет означать, то что стоит выше Animal в иерархии наследования
+        for (Animal animal : list) {
+            animal.eat();
+            //System.out.println(animal);
+        }
+    }
+}
