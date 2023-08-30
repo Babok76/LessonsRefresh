661b417f8fee9a1ab86c351a04d0b4c34773b930
Babok76 <skv767676@rambler.ru>
2023-08-30 19:27:29 +0300

    6

---
+++ src/Les35_Anonim_Class/Test.java
@@ -0,0 +1,50 @@
+package Les35_Anonim_Class;
+
+public class Test {
+    public static void main(String[] args) {
+        Animal anim1 = new Animal();
+        anim1.eat();
+
+/*
+        otherAnimal otherAnim = new otherAnimal();
+        otherAnim.eat();
+*/
+        Animal anim2 = new Animal() {
+            public void eat() {
+                System.out.println("Other Animal is eating");
+            }
+        };
+        //anim2.eat();
+        AbleToEat ableToeat = new AbleToEat.Animal();
+
+        AbleToEat able2 = new AbleToEat() { //создание анонимного класса на 1 раз
+            @Override
+            public void eat() {
+                System.out.println(".....");
+            }
+        };
+
+    }
+}
+
+interface AbleToEat{
+    public void eat();
+
+    class Animal implements AbleToEat{
+
+        @Override
+        public void eat() {
+            System.out.println("Animal is eating");
+        }
+    }
+}
+/*    class Animal {
+        public void eat() {
+            System.out.println("Animal is eating");
+        }
+    }*/
+
+/*class otherAnimal extends Animal{
+    public void eat(){
+        System.out.println("Other Animal is eating");
+    }*/
