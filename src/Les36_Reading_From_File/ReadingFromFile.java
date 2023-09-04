fcad8354e9b01881b4e8eb871762d4fb56b18155
Babok76 <skv767676@rambler.ru>
2023-09-04 16:43:07 +0300

    6

--- src/Les36_Reading_From_File/ReadingFromFile.java
+++ src/Les36_Reading_From_File/ReadingFromFile.java
@@ -2,6 +2,7 @@ package Les36_Reading_From_File;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadingFromFile {
@@ -10,18 +11,30 @@ public class ReadingFromFile {
         String value = scan1.nextLine();*/
 
         /////1. Создаем объект класса File
-        String separator = File.separator;
+/*        String separator = File.separator;
         String path = separator + "Users" + separator + "Ser" + separator + "Desktop" + separator + "TestForJava.txt";
         System.out.println(path);
-        File file1 = new File(path);
+        File file1 = new File(path);*/
+        File file1 = new File("testFile");
 
         /////2. Подаем на вход созданный файл
         Scanner scan1 = new Scanner(file1);
 
-        /////3. Выводим по одной строке
-        while (scan1.hasNextLine()) {
+        /////3.1 Выводим по одной строке. Если в файле уже все по строкам.
+/*        while (scan1.hasNextLine()) {
             System.out.println(scan1.nextLine());
+        }*/
+
+        /////3.2 раскладываем строку по элементам
+        String line = scan1.nextLine();  //загружаем строку
+        String [] numbersString = line.split(" "); //выбираем сепаратор для разделения строки
+        int [] numbersInt = new int[3];
+        int counter = 0;
+
+        for (String number:numbersString){
+            numbersInt [counter++] = Integer.parseInt(number);
         }
+        System.out.println(Arrays.toString(numbersInt));
 
         /////4. Обязательно закрываем поток
         scan1.close();
---
+++ src/Les37_Exception1/Exception1.java
@@ -0,0 +1,20 @@
+package Les37_Exception1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exception1 {
+    public static void main(String[] args)  {
+        File file = new File("123");
+
+        try {
+            Scanner scan = new Scanner(file);
+            System.out.println("До блока catch"); //строка не будет выполняться, если сработало Исключение
+        } catch (FileNotFoundException e) {
+            //throw new RuntimeException(e);
+            System.out.println("Файл не найден");
+        }
+        System.out.println("Код дальше выполняется");
+    }
+}
