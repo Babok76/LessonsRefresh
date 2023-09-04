5c2e6c43bf24df19505d7ca17614aab17e0d1b93
Babok76 <skv767676@rambler.ru>
2023-09-04 16:48:27 +0300

    6

--- src/Les37_Exception1/Exception1.java
+++ src/Les37_Exception1/Exception1.java
@@ -5,8 +5,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Exception1 {
-    public static void main(String[] args)  {
-        File file = new File("123");
+    public static void main(String[] args) {
+/*        File file = new File("123");
 
         try {
             Scanner scan = new Scanner(file);
@@ -16,5 +16,16 @@ public class Exception1 {
             System.out.println("Файл не найден");
         }
         System.out.println("Код дальше выполняется");
+    }*/
+        try {
+            readFile();
+        } catch (FileNotFoundException e) {
+            //throw new RuntimeException(e);
+            System.out.println("Обработка в main");
+        }
+    }
+    public static void readFile() throws FileNotFoundException{
+            File file = new File("123");
+            Scanner scan = new Scanner(file);
     }
 }
