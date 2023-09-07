c3263db2c000bc404100ea81627ef91217b468fe
Babok76 <skv767676@rambler.ru>
2023-09-07 16:43:35 +0300

    6

---
+++ src/Les43_Vlogenie_Class/Test.java
@@ -0,0 +1,10 @@
+package Les43_Vlogenie_Class;
+
+public class Test {
+    public static void main(String[] args) {
+        Electrocar elec1 = new Electrocar(1);
+        elec1.start();
+
+        Electrocar.Battery bat1 = new Electrocar.Battery();
+    }
+}
