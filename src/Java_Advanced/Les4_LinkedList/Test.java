cbe3b4c5ce782aaf19e574b097b43e4597f5cbb6
Babok76 <skv767676@rambler.ru>
2023-10-20 19:54:26 +0300

    6

---
+++ src/Java_Advanced/Les4_LinkedList/Test.java
@@ -0,0 +1,17 @@
+package Java_Advanced.Les4_LinkedList;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        //List<Integer> linkList = new LinkedList<>();
+        MyLinkedList my1 = new MyLinkedList();
+        my1.add(1);
+        my1.add(2);
+        my1.add(10);
+
+        System.out.println(my1);
+
+    }
+}
