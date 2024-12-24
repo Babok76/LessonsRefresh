738e5ee635b17fb207661f64a33038dd8282bfc1
Babok76 <skv767676@rambler.ru>
2024-12-24 15:39:14 +0300

    6

---
+++ src/Java_Ad2/Less1_ArrayList/Factorial.java
@@ -0,0 +1,17 @@
+package Java_Ad2.Less1_ArrayList;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+int sum =1;
+        for (int i = 1; i <=5; i++) {
+
+            sum *= i;
+            System.out.println(sum + " ");
+
+
+
+        }
+
+    }
+}
